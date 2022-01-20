33715c5f3139198ea0f6cd930117a3336fe078db
Nico Perez <nicolasperez@multiplica.com>
2022-01-20 12:14:56 -0300

    feat - new views (empty)

---
+++ blog.php
@@ -0,0 +1,202 @@
+<?php include('partials/header.php'); ?>
+
+<body>
+    <div class="stage mb-5">
+        <div class="menu">
+            <div class="container-xl">
+                <div class="row">
+                    <div class="col-lg-3 col-6 order-1 order-lg-1">
+                        <a href="index.php" class="stage__title">
+                            W Type Foundry
+                        </a>
+                    </div>
+                    <div class="col-lg-6 col-12 order-3 order-lg-2">
+                        <div class="menu__table">
+                            <div class="menu__tr">
+                                <div class="menu__td">
+                                    <a href="typefaces.php">
+                                        Typefaces
+                                    </a>
+                                </div>
+                                <div class="menu__td">
+                                    <a href="about.php">
+                                        About
+                                    </a>
+                                </div>
+                                <div class="menu__td">
+                                    <a href="blog.php">
+                                        Blog
+                                    </a>
+                                </div>
+                                <div class="menu__td">
+                                    <a href="licences.php">
+                                        Licences
+                                    </a>
+                                </div>
+                                <div class="menu__td">
+                                    <a href="contact.php">
+                                        Contact
+                                    </a>
+                                </div>
+                                <div class="menu__td">
+                                    <a href="">
+                                        Sign In
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6 order-2 order-lg-3 u-right">
+                        <a href="index.php" class="stage__logo">
+                            <svg data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 368.28" class="svg replaced-svg"><defs><style>.cls-1{fill:#1d1d1b;}</style></defs><title>LOGO WTF</title><path class="cls-1" d="M292.66,303.76c99.76-29,201.2-77.75,255.25-127.79v3.67c0,80.41-134.46,124.12-243.91,124.12Zm29.36-41c11.35-3.34,22.69-7,33.7-11V111.23c16,0,22,6.34,30,31.7h2.67c-.67-15.68,0-35.37,1.67-48H288.32c1.67,12.67,2.34,32.36,1.67,48h2c8-25.36,14-31.7,30-31.7ZM155.19,278.07c-51.72-19.69-95.76-52.39-95.76-98.77,0-31.36,21.35-56.39,49.71-75.07,4.34,4,6.68,10,9.68,22.69ZM131.5,330.12c34.37,0,74.41-5.34,115.45-14.68,19.68,2,38.37,3.67,58.39,3.67,117.11,0,266.6-49.05,266.6-139.47a87.2,87.2,0,0,0-4-25c12-16,19.69-30.7,19.69-47.05a69.22,69.22,0,0,0-1.34-11.67c0,115.78-324.65,212.54-459.78,214.21-54.39.67-90.76-22-90.76-57.73,0-11.34,2.67-21.68,9.67-34,17,32,54.73,56.72,100.77,73.41,12,0,34.37-1,45-2.34-9.35-2.67-19.36-5.34-28.7-8.67l27-114.12,28.36,119.45a14.68,14.68,0,0,0,5-1l37.71-158.15c4.34-19,11-26.36,25.69-29.7V94.89H229.93v2.33c14,3,17.35,10.68,12.68,29.7l-13.68,60.39-14.68-60.73c-4.67-19-1.34-26.69,12.34-29.36V94.89H172.54v1.66l8.34,33.7L167.54,187l-14.35-59.73c-4.34-18.68-.34-26.69,16-30V94.89h-44c49.72-26,118.12-39.38,178.84-39.38,58.4,0,128.46,13,177.85,39.38H392.09v2.33c16.35,3,21.36,8,21.36,21V229.69c11-5,22-10,32.69-15V172.29h3c13.34,0,18.68,4.34,22.69,18.36h2.33c-1.67-18.36-1.67-39.38,0-53.06h-2.33c-4,14-9,18-22.69,18h-3V111.23h3c24,0,31.7,4.68,37.7,24h2.34a217,217,0,0,1,1-36c25,14.68,44,32.7,52.38,53.72a156.25,156.25,0,0,0,17-16C534.23,93.55,471.17,64.86,403.1,50.84a500.8,500.8,0,0,1,71.74-5.67c20.35,0,38.37,1.67,54.05,5.34-20.35-8.68-47.38-12.35-78.07-12.35-25,0-52.39,2.67-80.42,7a506.12,506.12,0,0,0-67.73-4.67C182.55,40.5,35.4,88.55,35.4,178.63A115.17,115.17,0,0,0,36.74,195c-15.35,19-24.36,38-24.36,60.4C12.38,302.42,56.43,330.12,131.5,330.12Z"></path></svg>
+                        </a>
+                        <a href="" id="sandwich">
+                            +
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="blog mb-5" id="blogs">
+        <div class="container-xl">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="blog__post blog__post--big">
+                        <div class="col-md-8">
+                            <img src="https://picsum.photos/seed/picsum/1200/600" alt="Moncler">
+                        </div>
+                        <div class="col-md-4">
+                            <h3>
+                                <a href="" class="transition text text--blogtitle--big">
+                                    Nueva Merch
+                                    de temporada
+                                    verano WTF
+                                </a>
+                            </h3>
+                            <h5 class="text text--blogauthor--big">
+                                Team W - 01 . 2021
+                            </h5>
+                            <ul>
+                                <li>
+                                    <a href="" class="transition button">
+                                        Merch
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" class="transition button">
+                                        Vida
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" class="transition button button--circle">
+                                        +
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="blog__post blog__post--small">
+                        <img src="https://picsum.photos/seed/picsum/1200/600" alt="Moncler">
+                        <h4>
+                            <a href="" class="transition">
+                                Nueva Merch
+                                de temporada
+                                verano WTF
+                            </a>
+                        </h4>
+                        <h5>
+                            Team W - 01 . 2021
+                        </h5>
+                        <ul>
+                            <li>
+                                <a href="" class="button transition">
+                                    Interview
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="blog__post blog__post--small">
+                        <img src="https://picsum.photos/seed/picsum/1200/1200" alt="Moncler">
+                        <h4>
+                            <a href="" class="transition">
+                                Lorem ipsum dolor sit amet consectetur adipisicing, elit. Harum, non.
+                            </a>
+                        </h4>
+                        <h5>
+                            Team W - 01 . 2021
+                        </h5>
+                        <ul>
+                            <li>
+                                <a href="" class="button transition">
+                                    Interview
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="blog__post blog__post--small">
+                        <img src="https://picsum.photos/seed/picsum/1200/1600" alt="Moncler">
+                        <h4>
+                            <a href="" class="transition">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae aliquid ut sed tempore velit explicabo, vel cupiditate sequi odio voluptate.
+                            </a>
+                        </h4>
+                        <h5>
+                            Team W - 01 . 2021
+                        </h5>
+                        <ul>
+                            <li>
+                                <a href="" class="button transition">
+                                    Interview
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 u-center">
+                    <a href="" class="button button--black u-upper button--big">Ver más</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 mt-5">
+                    <section class="marquee button button--noborder button--big">
+                      <h4 class="text">
+                        Releases, freebies, secrets. Join our Newsletter***Releases, freebies, secrets. Join our Newsletter
+                      </h4>
+                      <h4 class="text">
+                        Releases, freebies, secrets. Join our Newsletter***Releases, freebies, secrets. Join our Newsletter
+                      </h4>
+                      <h4 class="text">
+                        Releases, freebies, secrets. Join our Newsletter***Releases, freebies, secrets. Join our Newsletter
+                      </h4>
+                      <h4 class="text">
+                        Releases, freebies, secrets. Join our Newsletter***Releases, freebies, secrets. Join our Newsletter
+                      </h4>
+                      <h4 class="text">
+                        Releases, freebies, secrets. Join our Newsletter***Releases, freebies, secrets. Join our Newsletter
+                      </h4>
+                      <h4 class="text">
+                        Releases, freebies, secrets. Join our Newsletter***Releases, freebies, secrets. Join our Newsletter
+                      </h4>
+                      <h4 class="text">
+                        Releases, freebies, secrets. Join our Newsletter***Releases, freebies, secrets. Join our Newsletter
+                      </h4>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<?php include('partials/footer.php'); ?>
