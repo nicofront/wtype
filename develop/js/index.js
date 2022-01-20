25fd303da486f095ba01ae9e3239cdd4142551d6
Nico Perez <nicolasperez@multiplica.com>
2022-01-20 12:17:25 -0300

    feat - slick slider

--- develop/js/index.js
+++ develop/js/index.js
@@ -36,4 +36,23 @@ ready(() => {
         dummy.init();
     }
 
+    $('.home--slider').slick({
+        arrows: false,
+        adaptiveHeight: false
+    });
+    
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#toTop').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+
+
+
 });
